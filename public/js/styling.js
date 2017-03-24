15d04d5d6f3a8673ddba98abb4f04674d3978a5c
Heenawter <hannah.k.mudge@gmail.com>
2017-03-23 21:48:56 -0600

    Fixed small positioning bug with add button

--- public/js/styling.js
+++ public/js/styling.js
@@ -1,45 +1,8 @@
 $(function() {
   handle_search();
-
-  //   var submitIcon = $('.searchbox-icon');
-  //   var inputBox = $('.searchbox-input');
-  //   var searchBox = $('.searchbox');
-  //   var isOpen = false;
-  //   submitIcon.click(function(){
-  //     if(isOpen == false){
-  //         searchBox.addClass('searchbox-open');
-  //         inputBox.focus();
-  //         isOpen = true;
-  //     } else {
-  //         searchBox.removeClass('searchbox-open');
-  //         inputBox.focusout();
-  //         isOpen = false;
-  //     }
-  //   });
-  //   submitIcon.mouseup(function(){
-  //         return false;
-  //   });
-  //   searchBox.mouseup(function(){
-  //         return false;
-  //   });
-  //   $(document).mouseup(function(){
-  //     if(isOpen == true){
-  //       $('.searchbox-icon').css('display','block');
-  //       submitIcon.click();
-  //     }
-  //   });
-  // });
-  //
-  // function buttonUp(){
-  //   var inputVal = $('.searchbox-input').val();
-  //   inputVal = $.trim(inputVal).length;
-  //   if( inputVal !== 0){
-  //     $('.searchbox-icon').css('display','none');
-  //   } else {
-  //     $('.searchbox-input').val('');
-  //     $('.searchbox-icon').css('display','block');
-  // }
 });
+
+//https://codepen.io/nikhil/pen/qcyGF
 
 function handle_search() {
   let searchBox = $('.search-bar');
