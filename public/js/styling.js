dd882c5cac35478ae15f2810223e05188ed79f67
Heenawter <hannah.k.mudge@gmail.com>
2017-03-26 20:21:14 -0600

    Made it so that if you click anywhere else on the page with the search bar open, it will close

--- public/js/styling.js
+++ public/js/styling.js
@@ -1,6 +1,5 @@
 $(function() {
   toggle_search();
-
 })
 
 //https://codepen.io/nikhil/pen/qcyGF
@@ -11,25 +10,35 @@ function toggle_search() {
   let input = $('.search-input');
   let isOpen = false;
 
-  submit.click(function() {
-    if(isOpen) {
-      let val = input.val();
-      if(val === "") {
-        isOpen = false;
-        searchBox.removeClass('search-bar-open');
-        searchBox.addClass('search-bar-closed');
-        input.focusout();
-      } else {
-        handle_search();
-      }
-    } else {
-      isOpen = true;
-      searchBox.removeClass('search-bar-closed');
-      searchBox.addClass('search-bar-open');
-    }
+  $(document).click(function(event) {
+      let target = $(event.target);
+      if(target.is(submit)) {
+        if(isOpen) {
+            let val = input.val();
+            if(val === "") {
+              isOpen = false;
+              searchBox.removeClass('search-bar-open');
+              searchBox.addClass('search-bar-closed');
+              input.focusout();
+            } else {
+              handle_search();
+            }
+          } else {
+            isOpen = true;
+            searchBox.removeClass('search-bar-closed');
+            searchBox.addClass('search-bar-open');
+          }
 
-    return false; //to prevent "form" from submitting
-  });
+          return false; //to prevent "form" from submitting
+      } else {
+        if(isOpen && !(target.is(input))) {
+          isOpen = false;
+          searchBox.removeClass('search-bar-open');
+          searchBox.addClass('search-bar-closed');
+          input.focusout();
+        }
+      }
+    });
 }
 
 function handle_search() {
