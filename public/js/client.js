cc3d7b70385ffe644f16494ef7c82f0d6994566f
Heenawter <hannah.k.mudge@gmail.com>
2017-03-30 20:29:44 -0600

    Experimenting with client

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,21 @@
 
 $(function() {
+  var socket = io.connect('http://127.0.0.1:3000');
+
+  socket.on('connect', function (data) {
+    socket.emit("get_albums", "test");
+  });
+
+  socket.on("get_albums", function(response) {
+    alert(response);
+  });
+
+  socket.on("get_album_images", function(response) {
+
+  }
+
+  socket.on("get_image", function(response) {
+
+  }
 
 });
