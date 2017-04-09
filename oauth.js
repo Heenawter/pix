4b24112b0f8e86f884798e1e7991cdbf392d2831
Brenda Pham <pham.bth@gmail.com>
2017-04-09 11:46:36 -0600

    added oauth.js - required to run passport

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+	google: {
+		clientID : '936032474766-p6t24c06i7llovtik4qkpbcqm33p5skh.apps.googleusercontent.com',
+		clientSecret : 'JTRIr7QSGF9mZ6RNLks9zXfZ',
+		callbackURL : "http://127.0.0.1:3000/auth/google/callback"
+	}
+};
+
+module.exports = ids;
